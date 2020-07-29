79f9a53fba217b0fcd6f459476c9d889f6be579d
Shang-Wei Tsai <wayne040904007@gmail.com>
2020-07-30 03:23:05 +0800

    fix: test normal type commit works

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ console.log('hello world');
 console.log('deploy npm on travis ci via semantic-release!');
 
 console.log('add normal commit');
+
+console.log('test other type works after setting custom commit');
