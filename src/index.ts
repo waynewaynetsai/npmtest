25d03b788550d8d430cd09ce7edc2f6d6d1a327b
Shang-Wei Tsai <wayne040904007@gmail.com>
2020-07-30 03:54:13 +0800

    set entry folder to src

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ console.log('deploy npm on travis ci via semantic-release!');
 console.log('add normal commit');
 
 console.log('test other type works after setting custom commit');
+
+function passlint() {
+ return 'lint';
+}
